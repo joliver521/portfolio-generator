bb294d62341d5c9d7f169ca06fc5cd0e0177dbfe
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-06-25 14:46:20 -0400

    inquirer promp

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
 // Short hand
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 // Long form
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
@@ -23,10 +18,25 @@ const [name, github] = profileDataArgs;
 
 // printProfileData(profileDataArgs);
 
+const inquirer = require('inquirer');
 
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message:'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
