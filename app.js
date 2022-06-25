dc92ff76b04684fafc61c7b0f13e1733aeb5799f
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-06-25 12:07:39 -0400

    node APIs and fs.writefiles used

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
-  // This...
-  // for (let i = 0; i < profileDataArr.length; i++) {
-  //   console.log(profileDataArr[i]);
-  // }
+const profileDataArgs = process.argv.slice(2);
 
-  // Is the same as this...
-  profileDataArr.foreach((profileItem) => console.log(profileItem));
-};
+// Short hand
+const [name, github] = profileDataArgs;
+// Long form
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-printProfileData(profileDataArgs);
+// console.log(profileDataArgs);
+
+// const printProfileData = (profileDataArr) => {
+// This...
+// for (let i = 0; i < profileDataArr.length; i++) {
+//   console.log(profileDataArr[i]);
+// }
+
+// Is the same as this...
+//   profileDataArr.foreach((profileItem) => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
