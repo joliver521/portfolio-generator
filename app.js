9059a894bb4691a23e58b0d7aa4ba2155dfd2673
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-06-26 10:28:06 -0400

    added about me section

--- app.js
+++ app.js
@@ -159,15 +159,10 @@ promptUser()
   .then(promptProject)
   .then((portfolioData) => {
     console.log(portfolioData);
+    // will be uncommented in lesson 4
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
   });
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
